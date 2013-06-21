3f98bddaec45b917811f30b2830106e2be2f5aaf
Rowan Thorpe <rowan@rowanthorpe.com>
2013-06-21 15:44:15 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 #
 # This file is part of [####]. See README.rst for more information.
 #
-# This setup.py and file hierarchy is from the setup-with-teeth project (v0.1.2):
+# This setup.py and file hierarchy is from the setup-with-teeth project (v0.1.3):
 #  https://github.com/rowanthorpe/setup-with-teeth
 #  replace all [####] with your own text
 
